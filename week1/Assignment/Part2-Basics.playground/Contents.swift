83aba895b9d85f79d37534f01c9bade5e907251e
chenshenyi <123dtw@gmail.com>
2023-08-29 17:20:45 +0800

    Rearange Structure

---
+++ week1/Assignment/Part2-Basics.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+// #2
+let pi: Double = Double.pi
+
+// #3
+let x: Int = 10
+let y: Int = 20
+let average: Int = (x + y) / 2
+
+// #4
+let average2: Double = Double(average)
+
+// #5
+let ten:Int = 10
+let three:Int = 3
+let remainder:Int = 10 % 3
+
+// #6
+var flag:Bool = true
+var inputString:String = "Hello world."
+let bitsInBite:Int = 8
+let averageScore:Double = 86.8
